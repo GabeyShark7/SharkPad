1e106002b51edbc0f7c27e604d6c61faaf6f8adf
GabeyShark76 <gabeyshark76@gmail.com>
2026-01-15 15:14:43 -0500

    .gitignore and requirments.txt updated.

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QPlainTextEdit
+
+#Application call
+app = QApplication(sys.argv)
+
+window = QMainWindow()
+#Setting window title
+window.setWindowTitle("Sharkpad")
+
+#Text editior widget
+editor = QPlainTextEdit()
+editor.setPlaceholderText("Sharkpad — start writing…")
+
+#Window sizing
+window.setCentralWidget(editor)
+window.resize(900, 600)
+window.show()
+
+sys.exit(app.exec())
